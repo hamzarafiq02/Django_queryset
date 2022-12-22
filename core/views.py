ee1ef89dcd62d5591cf7a342397d0577a4f214da
hamzarafiq02 <hamzarafiq9876@gmail.com>
2022-12-22 20:32:51 +0500

    final commit

--- core/views.py
+++ core/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from .models import Student
+from django.db.models import Avg, Min, Max, Count
 # Create your views here.
 
 
 def home(request):
     student_data = Student.objects.all()
+    print(student_data.query)
+
+    # Average = student_data.aggregate(Count('marks'))
+    # print(Average)
+
+    # Average = student_data.aggregate(Max('marks'))
+    # print(Average)
+
+    # Average = student_data.aggregate(Min('marks'))
+    # print(Average)
+
+    # Average = student_data.aggregate(Avg('marks'))
+    # print(Average)
 
     # student_data = Student.objects.filter(marks=90)
 
@@ -21,5 +35,7 @@ def home(request):
     # student_data = Student.objects.create(id='1', name='test', roll_no='16', city='lahore', marks='40', pass_date='2022-12-15')
 
     # student_data = Student.objects.filter(id='2').update()
+
+
 
     return render(request, 'core/home.html', {'students': student_data})
